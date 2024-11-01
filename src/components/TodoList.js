5acfda3fdc3a8c76d2145bf1a0e7d9b5949eda0e
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-11-02 00:42:19 +0530

    Use list rendering

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,38 @@
 import TodoItem from "./TodoItem";
 
+const todos = [
+    "Go Shopping",
+    "Do Homework",
+    "Do the assignment",
+    "Play cricket",
+]
+
 function TodoList() {
     return (
-        <div style={{ 
-            backgroundColor: "#f0f0f0", 
+        <div style={{
+            backgroundColor: "#f0f0f0",
             padding: "20px",
-            borderRadius: "10px", 
-            maxWidth: "400px", 
-            margin: "20px" }}>
+            borderRadius: "10px",
+            maxWidth: "400px",
+            margin: "20px"
+        }}>
 
-            <h1 style={{ 
-                color: "#333", 
-                textAlign: "center", 
-                fontSize: "24px", 
-                marginBottom: "20px" }}>
-                    My Todo List
+            <h1 style={{
+                color: "#333",
+                textAlign: "center",
+                fontSize: "24px",
+                marginBottom: "20px"
+            }}>
+                My Todo List
             </h1>
-            <TodoItem todo="Go Shopping" isCompleted={true} style={{ color: "green", fontWeight: "bold" }} />
+            {todos.map((todo) => {
+                return             <TodoItem todo={todo} isCompleted={true} style={{ color: "green", fontWeight: "bold" }} />
+            }      
+        )}
+            {/* <TodoItem todo="Go Shopping" isCompleted={true} style={{ color: "green", fontWeight: "bold" }} />
             <TodoItem todo="Do Homework" isCompleted={false} style={{ color: "red", textDecoration: "line-through" }} />
             <TodoItem todo="Do the assignment" isCompleted={true} style={{ color: "green", fontWeight: "bold" }} />
-            <TodoItem todo="Play cricket" isCompleted={false} style={{ color: "red", textDecoration: "line-through" }} />
+            <TodoItem todo="Play cricket" isCompleted={false} style={{ color: "red", textDecoration: "line-through" }} /> */}
         </div>
     )
 }
